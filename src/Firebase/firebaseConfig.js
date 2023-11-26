d163ac9d0126ff469db10c5cada61c528131387c
Saravanakumar R <100985347+Saravanakumar2003@users.noreply.github.com>
2023-11-26 17:04:37 +0530

    Removed firebaseConfig.js keys

--- src/Firebase/firebaseConfig.js
+++ src/Firebase/firebaseConfig.js
@@ -4,13 +4,13 @@ import { getFirestore } from 'firebase/firestore'
 // import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBsaYWjRI_AATZMHtZa51Fdf9RRZwWzbzs", 
-    authDomain: "empowerkids.firebaseapp.com",
-    projectId: "empowerkids",
-    storageBucket: "empowerkids.appspot.com",
-    messagingSenderId: "238442121491",
-    appId: "1:238442121491:web:cdd27f653831ef3959b2e6",
-    measurementId: "G-32321CBCL7"
+    apiKey: "", 
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   };
 
 // Initialize Firebase
